dccb696f56ab2780ba5dc9083fd475068e5b6239
GirishSrinivas <girish.s.srinivas@gmail.com>
2018-09-03 02:55:53 -0500

    adding new Sample spring mvc app called HelloApp with xml config

---
+++ HelloApp/src/main/java/com/girish/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.girish.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping(value="/greeting")
+	public String greeting(Model model) {
+		model.addAttribute("greeting", "Hello, Girish!...");
+		return "hello";
+	}
+}
